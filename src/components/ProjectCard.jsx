c9a976ef69e683951573a66ac4cd5d5d2b9bd453
Mejanur Rahman <mrmezan06@gmail.com>
2022-08-15 13:18:29 +0600

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
